08d6d01154d074a06356e5b4dff5dd15f3d4d5f8
Kevin <kevinalexanderduran8@gmail.com>
2022-02-28 21:57:57 -0800

    FINISHED

--- src/App.js
+++ src/App.js
@@ -24,21 +24,50 @@ function App() {
       })
   }
 
+  const takePhoto = () => {
+    const width = 414;
+    const height = width / (16/9);
+
+    let video = videoRef.current;
+    let photo = photoRef.current;
+
+    photo.width= width;
+    photo.height=height;
+
+    let ctx = photo.getContext('2d');
+    ctx.drawImage(video, 0, 0, width, height);
+
+    setHasPhoto(true);
+  }
+
+
+  const closePhoto = () => {
+    let photo = photoRef.current;
+    let ctx = photo.getContext('2d');
+
+    ctx.clearRect(0,0, photo.width, photo.height);
+
+    setHasPhoto(false);
+
+  }
+
+
   useEffect(() => {
     getVideo();
   },[videoRef])
 
 
+  
 
   return (
     <div className="App">
       <div className="camera">
         <video ref={videoRef}></video>
-        <button>SNAP!</button>
+        <button onClick={takePhoto}>SNAP!</button>
       </div>
-      <div className={'result' + (hasPhoto ? 'hasPhoto' : '')}>
+      <div className={'result ' + (hasPhoto ? 'hasPhoto' : '')}>
         <canvas ref={photoRef}></canvas>
-        <button>CLOSE!</button>
+        <button onClick={closePhoto}>CLOSE!</button>
       </div>
     </div>
   );
